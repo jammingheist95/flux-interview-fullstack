98711a0738172765f089136ffbe1af42acf0c25a
J0nathan-Andrew <jonathan@ifca.com.my>
2020-09-15 15:36:33 +0800

    added new fields and matrix grid, functionality still pending though buttons have been added. Styling also needed but need to follow methodology applied.

--- components/MatrixTable/index.tsx
+++ components/MatrixTable/index.tsx
@@ -1,56 +1,166 @@
-import classnames from 'classnames'
-import { useContext } from 'react'
-import { MatrixTableContext, MatrixTableContextProvider } from './context'
+import classnames from "classnames";
+import { useContext } from "react";
+import { MatrixTableContext, MatrixTableContextProvider } from "./context";
+import defaultPricing from "../../pages/api/pricing";
 
 type Props = {
-  initialMatrix?: import('../../types').Matrix
-} & import('react').HTMLAttributes<HTMLDivElement>
+  initialMatrix?: import("../../types").Matrix;
+} & import("react").HTMLAttributes<HTMLDivElement>;
 
 /**
- * Add 4 buttons: 
+ * Add 4 buttons:
  * - Cancel to reset the matrix to how it was before changing the values (only when in edit mode)
  * - Edit to make the fields editable (only when not in edit mode)
  * - Clear to completely clear the table
  * - Save to save the table
- * @param param0 
+ * @param param0
  */
-const MatrixTable: import('react').FC<Omit<Props, 'initialMatrix'>> = ({ className, children, ...props }) => {
+const MatrixTable: import("react").FC<Omit<Props, "initialMatrix">> = ({
+  className,
+  children,
+  ...props
+}) => {
   // State ------------------------------------------------------------------- //
-  const [{ matrix }, dispatch] = useContext(MatrixTableContext)
+  const [{ matrix }, dispatch] = useContext(MatrixTableContext);
 
   // Handlers ---------------------------------------------------------------- //
+
   // You can save (to api) the matrix here. Remember to update originalMatrix when done.
-  const save = async () => {
-
-  }
-
+  const save = async () => {};
+  const cancel = async () => {};
+  const edit = async () => {};
+  const clear = async () => {};
   // Effects ----------------------------------------------------------------- //
 
   // Rendering --------------------------------------------------------------- //
+
   return (
-    <div className={classnames(['container', className])} {...props}>
-      <button onClick={save}>Save</button>
+    <div className={classnames(["container", className])} {...props}>
+      <table>
+        <tr>
+          <th></th>
+          <th>Lite</th>
+          <th>Standard</th>
+          <th>Unlimited</th>
+        </tr>
+        <tr>
+          <td>36 months</td>
+          <td>
+            <input
+              placeholder="Enter value here"
+              value={matrix["36months"].lite || ""}
+              onChange={(e) =>
+                dispatch({
+                  type: "SOME_ACTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </td>
+          <td>
+            <input
+              placeholder="Enter value here"
+              value={matrix["36months"].standard || ""}
+              onChange={(e) =>
+                dispatch({
+                  type: "SOME_ACTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </td>
+          <td>
+            <input
+              placeholder="Enter value here"
+              value={matrix["36months"].unlimited || ""}
+              onChange={(e) =>
+                dispatch({
+                  type: "SOME_ACTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>24 months</td>
+          <input
+            placeholder="Enter value here"
+            value={matrix["24months"].lite || ""}
+            onChange={(e) =>
+              dispatch({
+                type: "SOME_ACTION",
+                payload: e.target.value,
+              })
+            }
+          />
+          <td>
+            <input
+              placeholder="Enter value here"
+              value={matrix["24months"].standard || ""}
+              onChange={(e) =>
+                dispatch({
+                  type: "SOME_ACTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </td>
+          <td>
+            <input
+              placeholder="Enter value here"
+              value={matrix["24months"].unlimited || ""}
+              onChange={(e) =>
+                dispatch({
+                  type: "SOME_ACTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </td>
+        </tr>
+      </table>
+      {/* <button onClick={save}>Save</button>
+      <br />
+      <br /> */}
+      {/* 36 months lite:
+      <input
+        placeholder="Enter value here"
+        value={matrix["36months"].lite || ""}
+        onChange={(e) =>
+          dispatch({
+            type: "SOME_ACTION",
+            payload: e.target.value,
+          })
+        }
+      /> */}
+      <div
+        style={{
+          display: "flex",
+          margin: "auto",
+          width: "50%",
+          padding: "10px",
+        }}
+      >
+        <button onClick={edit}>Edit</button>
+        <button onClick={save}>Save</button>
+        <button onClick={cancel}>Cancel</button>
+        <button onClick={clear}>Clear</button>
+      </div>
       <br />
       <br />
-
-      36months lite:
-      <input 
-        value={matrix["36months"].lite || ''} 
-        onChange={(e) => dispatch({
-          type: 'SOME_ACTION',
-          payload: e.target.value
-        })} />
-
       <style jsx>{`
         .container {
-          
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-const MatrixTableWithContext: import('react').FC<Props> = ({ initialMatrix, ...props }) => {
+const MatrixTableWithContext: import("react").FC<Props> = ({
+  initialMatrix,
+  ...props
+}) => {
   // You can fetch the pricing here or in pages/index.ts
   // Remember that you should try to reflect the state of pricing in originalMatrix.
   // matrix will hold the latest value (edited or same as originalMatrix)
@@ -59,7 +169,7 @@ const MatrixTableWithContext: import('react').FC<Props> = ({ initialMatrix, ...p
     <MatrixTableContextProvider initialMatrix={initialMatrix}>
       <MatrixTable {...props} />
     </MatrixTableContextProvider>
-  )
-}
+  );
+};
 
-export default MatrixTableWithContext
+export default MatrixTableWithContext;
